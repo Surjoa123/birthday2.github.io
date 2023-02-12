13a5b126d1d0ac9dcb1363f24c77a522630cf055
Surjoa123 <124343205+Surjoa123@users.noreply.github.com>
2023-02-12 13:56:37 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -15,7 +15,7 @@ var phrases = ["Click Me 😇", "Happy Birthday! 🥳", "You are Awesome 😍", 
   // bImages = ["https://i.imgur.com/Q2ZST9f.jpeg", "https://i.imgur.com/saf2C0e.gif","https://i.imgur.com/Hdix4fN.jpg",
   //     "https://i.imgur.com/mYcAgr3.png", 
   //     "https://i.imgur.com/Hdix4fN.jpg", "https://i.imgur.com/4clqUdj.jpg","https://i.imgur.com/binfCky.jpg", "https://ak.picdn.net/shutterstock/videos/3153106/thumb/1.jpg", "https://birthdaycake24.com/uploads/worigin/2019/06/11/ happy-birthday-cake-with-name-birthdaycake245cff57627fae2_1239920b09a2d01c20f8265f4a0e3efd.gif"];
-  bImages = ["/img/img1.jpeg","/img/last.gif","/img/img1.1.jpeg","/img/img2.jpeg","/img/img7.1.png","/img/img11.png","/img/img15.jpeg","/img/img14.png","/img/img5.jpeg","/img/img12.jpeg","/img/img10.jpeg","/img/img3.jpeg"]
+  bImages = ["./img/img1.jpeg","./img/last.gif","./img/img1.1.jpeg","./img/img2.jpeg","./img/img7.1.png","./img/img11.png","./img/img15.jpeg","./img/img14.png","./img/img5.jpeg","./img/img12.jpeg","./img/img10.jpeg","./img/img3.jpeg"]
 
   
  var count = 0;
